b5e1c93a853469cbcca3462a2ffa95208d91a70b
Veronica <veronica.maizlin@gmail.com>
2019-01-10 21:16:02 -0600

    removed comments

--- test/test.js
+++ test/test.js
@@ -3,14 +3,13 @@ const assert = require('assert');
 describe('Page opening', function () {
 
     it('get title', function(){
-        browser.url('/'); //open baseUrl + string passed in .url() function
-        let title = browser.getTitle(); //get page title and assign it to the "title" variable
-        assert.equal(title, 'Bug Tracker', 'Title is incorrect'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
+        browser.url('/');
+        let title = browser.getTitle();
+        assert.equal(title, 'Bug Tracker', 'Title is incorrect');
     })
-
     it('get header title', function(){
-        let title = browser.getTitle(); //get page title and assign it to the "title" variable
-        assert.equal(title, 'Bug Tracker', 'Title is incorrect'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
+        let title = browser.getTitle();
+        assert.equal(title, 'Bug Tracker', 'Title is incorrect');
     })
 
 });
